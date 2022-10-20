1848160bdbbb24b01a1241bb50805ed412577b0c
tizx <tizi.marabotto@gmail.com>
2022-10-20 08:52:10 -0300

    git commit -m "agregado de contador tiempo"

--- proyecto.cpp
+++ proyecto.cpp
@@ -4,9 +4,24 @@
 
 #include "proyecto.h"
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main(){
+    clock_t begin;
+    cout << "Comenzando a medir Tiempo\n" << endl;
+
+    begin = clock();
+    /*
+     * * Insertar código a medir tiempo aquí
+     */
+    clock_t end = clock();
+
+    double elapsed_secs = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
+
+    cout << "Tardo elapsed_secs" << elapsed_secs << "\n" << std::endl;
+
     cout << "hola como estas" << endl;
     return 0;
 }
+

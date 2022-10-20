4a97699d18a92f637dffd2bdb203c4e04df0fcc8
melaniefarneti <2113289@ucc.edu.ar>
2022-10-19 22:53:46 -0300

    subo cmake

---
+++ proyecto.cpp
@@ -0,0 +1,12 @@
+//
+// Created by farne on 19/10/2022.
+//
+
+#include "proyecto.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hola como estas" << endl;
+    return 0;
+}
